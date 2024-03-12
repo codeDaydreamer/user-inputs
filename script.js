0c6e4c29298939657e8e43e89659f6693c7641b9
Code Daydreamer <156774619+codeDaydreamer@users.noreply.github.com>
2024-03-12 22:26:27 +0300

    Update script.js

--- script.js
+++ script.js
@@ -17,5 +17,15 @@ document.getElementById(`submitbtn`).onclick = function(){
     username2 = document.getElementById(`username2`).value;
     document.getElementById(`h2`).textContent= `Hello there ${username2} this is your alternative username`;
 }
+//the window alert accepts tet so we have to convert some inputs such as age and points.
+
+let age = window.prompt(`Input your age`);
+age = Number(age);
+newAge = age += 1;
+
+document.getElementById(`p1`).textContent=`One year from now you will be ${newAge} years old `
+console.log(typeof age);
+//converting a string to a number will give a NaN error meaning not a number
+//converting a string to a boolean will give a typeof true due to the prescenve of some values
 
 
